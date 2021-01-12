bda11f2c5c5ed0fa2a2f525b0211016fcb0b4601
Ifan Fairuz <ifanfairuz@gmail.com>
2021-01-12 15:36:50 +0700

    [DONE]

--- App.js
+++ App.js
@@ -1,14 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
+import Index from './lib/scene';
+import { StyleProvider, Root } from 'native-base';
+import getTheme from './native-base-theme/components';
+import material from './native-base-theme/variables/material';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isReady: false,
+    };
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      Inconsolata: require('./assets/fonts/Inconsolata/Inconsolata.ttf'),
+      ...Ionicons.font,
+    });
+    this.setState({ isReady: true });
+  }
+
+  render() {
+    if (!this.state.isReady) {
+      return <AppLoading />;
+    }
+
+    return (
+      <NavigationContainer>
+        <StyleProvider style={getTheme(material)}>
+          <Root>
+            <Index />
+          </Root>
+        </StyleProvider>
+      </NavigationContainer>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
